a524838a2ff43ccbe534991fb049e950e7fe30a4
architaagr25 <archita.agarwal25@gmail.com>
2025-05-29 04:31:55 +0530

    download update

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -12,24 +12,24 @@ const Hero = () => {
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold ">
                         <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-600'>
                             Hey, I'm {""}
-                            </span>
+                        </span>
 
-                            <br></br>
-                       <TypeAnimation
-              sequence={[
-                "Archita",
-                1000,
-                "Frontend Developer",
-                1000,
-                "Tech Enthusiast",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              repeat={Infinity}
-              className='text-white'
-            />
-          
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                "Archita",
+                                1000,
+                                "Frontend Developer",
+                                1000,
+                                "Tech Enthusiast",
+                                1000,
+                            ]}
+                            wrapper="span"
+                            speed={50}
+                            repeat={Infinity}
+                            className='text-white'
+                        />
+
                     </h1>
 
                     <p className='text-[#ADB7BE] text-base  mb-6 sm:text-lg lg:text-xl '>
@@ -41,25 +41,31 @@ const Hero = () => {
                     <div>
                         <button className='px-6 w-full sm:w-fit py-3 rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 hover:-translate-y-1 transition-transform text-white'>
                             Hire Me
-                             </button>
-                        <button className='px-1 py-1 w-full sm:w-fit rounded-full  hover:bg-slate-800 text-white mt-3 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:-translate-y-1 transition-transform'> 
-                            <span className='block bg-[#181818] hover:bg-slate-800 rounded-full px-5 py-2'>
-                                Download Resume 
-                            </span>
-                            </button>
+                        </button>
+                        <button className='px-1 py-1 w-full sm:w-fit rounded-full  hover:bg-slate-800 text-white mt-3 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500  hover:-translate-y-1 transition-transform'>
+                            <a
+                                href="/resume.pdf"
+                                download
+                            >
+                                <span className="block bg-[#181818] hover:bg-slate-800 rounded-full px-5 py-2">
+                                    Download Resume
+                                </span>
+                            </a>
+
+                        </button>
                     </div>
 
                 </div>
                 <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
                     <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative '>
                         <Image
-                    src= "/images/1hero.png"
-                    alt="hero image"
-                    className='absolute transform -translate-x-1/2
+                            src="/images/1hero.png"
+                            alt="hero image"
+                            className='absolute transform -translate-x-1/2
                      -translate-y-1/2 top-1/2 left-1/2'
-                    width = {300}
-                    height = {300}
-                    />
+                            width={300}
+                            height={300}
+                        />
                     </div>
                 </div>
             </div>
